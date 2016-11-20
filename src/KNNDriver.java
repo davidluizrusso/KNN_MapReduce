9015e3f4bd1e3fb09cd65ab1ef232e15174edfd2
davidluizrusso <david.luiz.russo@gmail.com>
2016-11-19 19:10:28 -0800

    changed driver organization

--- src/KNNDriver.java
+++ src/KNNDriver.java
@@ -13,24 +13,32 @@ public class KNNDriver {
 	
 	public static void main(String[] args) throws Exception {
 	    
+		// iniate configuration and job
 		Configuration conf = new Configuration(); 
-		conf.set("testdata", args[1]);
-		conf.set("traindata", args[2]);
-		
 	    Job job = new Job(conf, "PokerHand");
+
+	    // set testing and training data parameters within the job
+		job.getConfiguration().set("testdata", args[1]);
+		job.getConfiguration().set("traindata", args[2]);
 	    
+		// establish the driver, mapper, and reducer classes
+		job.setJarByClass(KNNDriver.class);
+		job.setMapperClass(KNNMapper.class);
+	    job.setReducerClass(KNNReducer.class);
+	    
+	    // establish output key value classes
 	    job.setOutputKeyClass(Text.class);
 	    job.setOutputValueClass(IntWritable.class);
 	        
-	    job.setMapperClass(KNNMapper.class);
-	    job.setReducerClass(KNNReducer.class);
-	        
+	    // establish inut and output format classes
 	    job.setInputFormatClass(TextInputFormat.class);
 	    job.setOutputFormatClass(TextOutputFormat.class);
 	        
-	    FileOutputFormat.setOutputPath(job, new Path(args[3]));
+	    // establish input and output directories
+		FileInputFormat.setInputPaths(job, new Path(args[3]));
+	    FileOutputFormat.setOutputPath(job, new Path(args[4]));
 	        
-	    job.setJarByClass(KNNDriver.class);
+	    // management parameter set
 	    job.waitForCompletion(true);
 	    
 	 } // end Main method
